47804b8bd131051c19f89bba2d4305334720ae11
SHANTO <rabbymd55555@gmail.com>
2025-09-24 19:52:32 +0600

    added firebase project and module 49 complete

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,20 +1,66 @@
-import React from "react";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import React, { useState } from "react";
+import {
+  getAuth,
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 import { auth } from "../../Firebase/Firebase.init";
 const Login = () => {
- const provider = new GoogleAuthProvider();
+  const [user, setUser] = useState(null);
+  const provider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
   const handleGoogleSignIn = () => {
     signInWithPopup(auth, provider)
-  .then((result) => {
-    console.log(result);
-  }).catch((error) => {
-    console.log(error);
-  });
+      .then((result) => {
+        console.log(result.user);
+        setUser(result.user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleRemove = () => {
+    const auth = getAuth();
+    signOut(auth)
+      .then(() => {
+        setUser(null);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleGithubSignIn = () => {
+    signInWithPopup(auth, githubProvider)
+      .then((result) => {
+        setUser(result.user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
   return (
     <div>
       <h1>Please Login</h1>
-      <button onClick={handleGoogleSignIn}>sign In With Google</button>
+
+      {user ? (
+        <button onClick={handleRemove}>Sign Out</button>
+      ) : (
+        <>
+          <button onClick={handleGoogleSignIn}>sign In With Google</button>
+          <button onClick={handleGithubSignIn}>Sign In with Github</button>
+        </>
+      )}
+      {user && (
+        <div>
+          <h3>User Name: {user.displayName}</h3>
+          <h3>Email : {user.email}</h3>
+          <img src={user.photoURL} alt="" />
+        </div>
+      )}
     </div>
   );
 };
